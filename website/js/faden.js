0470da18e1592ac0a5a724968dc5e0317894ab01
DXane <32098067+DXane@users.noreply.github.com>
2020-05-20 00:01:55 +0200

    Alle Buttons vereinheilicht und alles auf die selbe Breite gebracht

--- website/js/faden.js
+++ website/js/faden.js
@@ -1,31 +1,47 @@
 var sav;
-function vote(richtung){
-    if(richtung==='up' || richtung==1){
-        var count=document.getElementsByTagName('count');
+function vote(id){
+    var richtung = id.innerText === "Hoch" ? 'up' : 'down';
+    var count=document.getElementsByTagName('count');
+    var change;
+    if(richtung==='up'){
         var number = parseInt(count[0].innerHTML);
+        id.style.background="green"
+        id == document.getElementById('runter') ? 0 : document.getElementById('runter').style.background='none';
         if(sav==1){
-            number=number+2;
+            change=2;
             sav=0;
+
         }
         else if(sav === undefined){
-            number+=1;
+            change=1;
             sav=0;
         }
-        count[0].innerHTML=number;
+        else{
+            sav=undefined;
+            change=-1;
+            id.style.background="none";
+        }
     }
-    else if(richtung==='down' || richtung==0){
-        var count=document.getElementsByTagName('count');
+    else if(richtung==='down'){
         var number = parseInt(count[0].innerHTML);
+        id.style.background="red";
+        id == document.getElementById('hoch') ? 0 : document.getElementById('hoch').style.background='none';
         if(sav === undefined){
-            number=number-1;
+            change=-1;
             sav=1;
         }
         else if(sav==0){
-            number=number-2;
+            change=-2;
             sav=1;
         }
-        count[0].innerHTML=number;
+        else{
+            change=1;
+            sav=undefined;
+            id.style.background="none";
+        }
     }
+    number+=change;
+    count[0].innerHTML=number;
 }
 
 function sendkomment(){
@@ -35,7 +51,10 @@ function sendkomment(){
     if(komment.length!=0){
         insertKomment(komment);
     }
-    return komment;
+    else{
+        alert("Bitte schreibe einen Kommentar.")
+    }
+    consol.log(komment);
 }
 
 function insertKomment(komment){
@@ -47,3 +66,15 @@ function generateKomment(Komment){
     var string="<kommentar><ktitel>anonymous "+new Intl.DateTimeFormat('de', { month: 'short', day: '2-digit' }).format(new Date())+"</ktitel><br><inhalt>"+Komment+"</inhalt></kommentar>";
     return string;
 }
+
+$(document).ready(function(){
+    $("#edit").click(function(){
+        $('#about').prop('disabled', !$('#about').prop('disabled'));
+        if($('#edit').text()!='Speichern'){
+            $('#edit').text("Speichern");
+        }
+        else{
+            $('#edit').text("Bearbeiten");
+        }
+    });
+});
--- website/js/global.js
+++ website/js/global.js
@@ -1,56 +1,16 @@
 
-{
-    'faden'[
-        {
-           'titel':'Fäden 1111','comments':'1111', 'datum':'01.01.1970', 'inhalt':'Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt\'ger Schnelle Vom Himmel durch die Welt zur Hölle. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was denken lassen.' 
-            
-        },
-        {
-            'titel':'Fäden 1111', 'datum':'01.01.1970', 'inhalt':'Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt\'ger Schnelle Vom Himmel durch die Welt zur Hölle. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was denken lassen.' 
-             
-         },
-         {
-            'titel':'Fäden 1111', 'datum':'01.01.1970', 'inhalt':'Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt\'ger Schnelle Vom Himmel durch die Welt zur Hölle. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was denken lassen.' 
-             
-         },
-         {
-            'titel':'Fäden 1111', 'datum':'01.01.1970', 'inhalt':'Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt\'ger Schnelle Vom Himmel durch die Welt zur Hölle. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was denken lassen.' 
-             
-         },
-         {
-            'titel':'Fäden 1111', 'datum':'01.01.1970', 'inhalt':'Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt\'ger Schnelle Vom Himmel durch die Welt zur Hölle. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was denken lassen.' 
-             
-         },
-         {
-            'titel':'Fäden 1111', 'datum':'01.01.1970', 'inhalt':'Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt\'ger Schnelle Vom Himmel durch die Welt zur Hölle. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was denken lassen.' 
-             
-         }
-    ]
-
-}
-
-
-function insertfaden(anzahl){
-    var titel ="<div class=\"p-2 text-truncate\">Test</div>";
-    var datum ="<div class=\"p-2\" >01.01.1970</div>";
-    var kommentare="<div class=\"p-2\" style=\"width: 10%;\">Fäden 1111</div>";
-    var titelbox="<div class=\"d-flex box fadentitel\">"+titel+datum+kommentare+"</div>";
-
-    var inhalt="<div class=\"p-2  text-truncate\" style=\"width: 80%;\">Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt'ger Schnelle Vom Himmel durch die Welt zur Hölle. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was denken lassen.</div>";
-    var link="<div class=\"p-2\" style=\"width: 15%;\"><a href=\"faden.html\" class=\"box fadenbox\">    Zum Faden</a></div>";
-    var inhaltbox="<div class=\"d-flex flex-row justify-content-between fadeninhalt\">"+inhalt+link+"</div>";
+//Füge Fädeneinträge in den Container ein
+function insertfaden(anzahlfaden){
+    
+    if (anzahlfaden === undefined) {
+        anzahlfaden = 8;
+    }
 
     var list=document.getElementsByTagName("fadencontainer");
     var container=list[0];
-    for(i=0; i<arguments[0];i++){
-        container.innerHTML+="<div class=\"container-fluid faden box\">"+titelbox+inhaltbox+"</div>";
+    for(i=0; i<anzahlfaden;i++){
+        container.innerHTML+=generateFaden();
     }
-}
-
-function insertfaden_jquery(anzahl){
-    var title = $('<div>',{class:'p-2 text-truncate'});
-    title.text
-
 }
 
 function insertBeitrag(anzahlfaden){
@@ -74,7 +34,7 @@ function generateContentB(){
     var inhalt =" So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Es irrt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Es irrt der Mensch, wenn er sie beim Kragen hätte. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Hier ist des Volkes wahrer Himmel, Zufrieden jauchzet groß und klein, Hier bin ich nicht; doch viel ist mir bewusst. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Ich bin von je der Ordnung Freund gewesen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt'ger Schnelle Vom Himmel durch die Welt zur Hölle! Es irrt der Mensch, wenn er sie beim Kragen hätte. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt'ger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Es irrt der Mensch, wenn er sie beim Kragen hätte.";
     var string="<div class=\"boxcontent\"><p class=\"description\">Er hat gespunnen:</p>";
     string+="<div class=\"fadeninbox\">"+inhalt+"</div>";
-    string+="<a class=\"fadenlink\" href=\"./faden.html\">Zum Faden</a></div></div>";
+    string+="<a href=\"./faden.html\"><button class=\"btn-primary btn fadenlink\">Zum Faden</button></a></div></div></div>";
     return string;
 }
 
@@ -82,7 +42,7 @@ function generateContentB(){
 function generateFaden(){
     var inhalt="Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. Ich bin von je der Ordnung Freund gewesen. Gewöhnlich glaubt der Mensch, wenn er sie beim Kragen hätte. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächt'ger Schnelle Vom Himmel durch die Welt zur Hölle. Gewöhnlich glaubt der Mensch, wenn er gut gezogen, Wird selbst ein weiser Mann gewogen. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens Quelle hin. Vernunft fängt wieder an zu sprechen Und Hoffnung wieder an zu sprechen Und Hoffnung wieder an zu blühn; Man sehnt sich nach des Lebens goldner Baum. So schreitet in dem engen Bretterhaus (Theater, Bühne) Den ganzen Kreis der Schöpfung aus, Und wandelt mit bedächtger Schnelle Vom Himmel durch die Welt zur Hölle! Ich bin Ein Teil von jener Kraft, Die stets das Gute schafft. Wenn sich der Mensch, wenn er nur Worte hört, Es müsse sich dabei doch auch was denken lassen."
     var datum =new Intl.DateTimeFormat('de', { month: 'short', day: '2-digit' }).format(new Date())
-    var faden="<div class=\"container-fluid faden box\">"+"<div class=\"d-flex box fadentitel\">"+generateTitel("titel",datum,Math.round(Math.random()*1000))+"</div>";
+    var faden="<div class=\"container-fluid faden box\">"+generateTitel("titel",datum,Math.round(Math.random()*1000));
     faden+="<div class=\"d-flex flex-row justify-content-between fadeninhalt\">"+generateContent(inhalt,"faden.html")+"</div>";
     return faden;
 }
@@ -93,7 +53,7 @@ function generateTitel(titel="none",datum="",kommentare=""){
 
 //Erstelle die Inhaltbox mit den übergebenen Inhalt und Link zum Faden
 function generateContent(inhalt="",link=""){
-    return "<div class=\"p-2  text-truncate\" style=\"width: 80%;\">"+inhalt+"</div><div class=\"p-2\" style=\"width: 15%;\"><a href=\""+link+"\" class=\"box fadenbox\">    Zum Faden</a></div>";
+    return "<div class=\"p-2  text-truncate\" style=\"width: 80%;\">"+inhalt+"</div><div class=\"p-2\" style=\"width: 15%;\"><a href=\""+link+"\"><button class=\"btn btn-primary\">Zum Faden</button></a></div>";
 }
 
 $(document).ready(function(){
