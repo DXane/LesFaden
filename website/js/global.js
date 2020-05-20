21d6c70daf48992c77f3d9fc4669799d15fde67a
DXane <32098067+DXane@users.noreply.github.com>
2020-05-20 23:56:05 +0200

    Beiträge im Profil

--- website/js/global.js
+++ website/js/global.js
@@ -15,7 +15,7 @@ function insertfaden(anzahlfaden){
 
 function insertBeitrag(anzahlfaden){
     if (anzahlfaden === undefined) {
-        anzahlfaden = 0;
+        anzahlfaden = 3;
     }
     var list=document.getElementsByTagName("content");
     var content=list[0];
